04cc6f3f4232df20474317954c245d70e0115a00
adiMallya <adimallya99@gmail.com>
2024-08-18 22:26:00 +0530

    ADD: A Todo class to manage ops

---
+++ todo.js
@@ -0,0 +1,43 @@
+class Todo {
+    constructor(init = []) {
+        this.todoList = init;
+        this.id = init?.length;
+    }
+
+    add(todo) {
+        const newTask = {
+            id: this.id++,
+            task: todo,
+            completed: false
+        }
+        this.todoList = [...this.todoList, newTask];
+    }
+
+    remove(id) {
+        this.todoList = this.todoList.filter((item, _) => item.id !== id);
+    }
+
+    update(id, updatedTodo) {
+        this.todoList = this.todoList.map((item, idx) => item.id === id ? { ...item, task: updatedTodo } : item);
+    }
+
+    getAll() {
+        return this.todoList;
+    }
+
+    get(id) {
+        return this.todoList.find((item, _) => item.id === id) ?? null;
+    }
+
+    clear() {
+        this.todoList = [];
+    }
+
+    toggleComplete(id) {
+        this.todoList = this.todoList.map(item =>
+            item.id === id ? { ...item, completed: !item.completed } : item
+        )
+    }
+}
+
+export default Todo;
