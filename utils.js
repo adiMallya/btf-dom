6776190558235c2123550cea54d5c433d35a903d
adiMallya <adimallya99@gmail.com>
2024-08-20 23:08:05 +0530

    ADD: Date, groupBy methods

---
+++ utils.js
@@ -0,0 +1,24 @@
+
+export const groupTasksByDate = (todos = []) => {
+    return todos.reduce((groups, task) => {
+        const date = formatDate(task?.date);
+
+        if (!groups[date]) {
+            groups[date] = [];
+        }
+
+        groups[date] = [...groups[date], task];
+        return groups;
+    }, {});
+}
+
+export function formatDate(date) {
+    const dateToFormat = new Date(date);
+
+    if (dateToFormat.toDateString() === new Date()?.toDateString())
+        return "Today";
+
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    return dateToFormat.toLocaleDateString('en-IN', options);
+}
+
